5c83ad974c94abc32f3d158e67245e813a2ea551
Austin D'Acquisto <austindacquisto@gmail.com>
2024-06-07 11:32:51 -0700

    Merges with Blake's code for IMU.

--- include/IMUController.h
+++ include/IMUController.h
@@ -1,6 +1,7 @@
 #ifndef IMUCONTROLLER_H
 #define IMUCONTROLLER_H
 
+#include <Wire.h>
 class IMUController {
 public:
   IMUController();  // Constructor
@@ -8,9 +9,20 @@ public:
 
   // Add your member functions here
   void init();
+  String read();
 
 private:
   // Add your member variables here
+  const int MPU_ADDR = 0x68; // I2C address of the MPU-6050. If AD0 pin is set
+                             // to HIGH, the I2C address will be 0x69.
+  int16_t accelerometer_x, accelerometer_y,
+      accelerometer_z;            // variables for accelerometer raw data
+  int16_t gyro_x, gyro_y, gyro_z; // variables for gyro raw data
+  int16_t temperature;            // variables for temperature data
+  char tmp_str[7];                // temporary variable used in convert function
+
+  // Private helper functions
+  String convert_int16_to_str(int16_t i);
 };
 
 #endif // IMUCONTROLLER_H
--- src/main.cpp
+++ src/main.cpp
@@ -13,6 +13,7 @@ SDController sdController(SD_CS_PIN);
 
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(9600);
   imuController.init();
   servoController.init();
 
@@ -22,9 +23,9 @@ void setup() {
     Serial.println("SD card initialization failed.");
     return;
   }
-  
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  Serial.println(imuController.read());
 }
