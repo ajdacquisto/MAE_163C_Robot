4c145990115a207cae5dbf833e021dc415d50fc9
Austin D'Acquisto <austindacquisto@gmail.com>
2024-06-11 22:41:14 -0700

    An adjustment to matrix definitions to allow other matrix types to be used in the future.

--- include/Matrix.h
+++ include/Matrix.h
@@ -1,30 +1,30 @@
 #ifndef MATRIX_H
 #define MATRIX_H
 
-class Matrix3x1;  // Forward declaration
+class Matrix3x1 {
+public:
+  float data[3];
+
+  Matrix3x1();
+  Matrix3x1(const float arr[3]);
+
+  Matrix3x1 operator+(const Matrix3x1 &other) const;
+  Matrix3x1 operator-(const Matrix3x1 &other) const;
+  Matrix3x1 operator*(float scalar) const;
+};
 
 class Matrix3x3 {
 public:
-    float data[3][3];
+  float data[3][3];
 
-    Matrix3x3();
-    Matrix3x3(float init_data[3][3]);
-    Matrix3x3 operator+(const Matrix3x3 &other) const;
-    Matrix3x3 operator-(const Matrix3x3 &other) const;
-    Matrix3x3 operator*(const Matrix3x3 &other) const;
-    Matrix3x1 operator*(const Matrix3x1 &other) const;
-    Matrix3x3 transpose() const;
-};
+  Matrix3x3();
+  Matrix3x3(const float arr[3][3]);
 
-class Matrix3x1 {
-public:
-    float data[3];
-
-    Matrix3x1();
-    Matrix3x1(float init_data[3]);
-    Matrix3x1 operator+(const Matrix3x1 &other) const;
-    Matrix3x1 operator-(const Matrix3x1 &other) const;
-    Matrix3x1 operator*(float scalar) const;
+  Matrix3x3 operator+(const Matrix3x3 &other) const;
+  Matrix3x3 operator-(const Matrix3x3 &other) const;
+  Matrix3x3 operator*(const Matrix3x3 &other) const;
+  Matrix3x1 operator*(const Matrix3x1 &other) const;
+  Matrix3x3 transpose() const;
 };
 
 #endif // MATRIX_H
